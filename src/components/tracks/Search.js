dd12774f521a3306400e062921ecb610c1f5dae0
smishra11 <msubhasish11@gmail.com>
2020-11-06 12:14:36 +0530

    modified search component

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -1,70 +1,135 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Consumer } from '../../context';
+import Mic from '../layout/mic.svg';
+import Voice from '../layout/voice.svg';
 
-class Search extends Component {
-  state = {
-    trackTitle: '',
-  };
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
 
-  findTrack = (dispatch, e) => {
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = 'en-US';
+
+function Search1() {
+  const [trackTitle, setTrackTitle] = useState('');
+  const [isListening, setIsListing] = useState(false);
+
+  useEffect(() => {
+    handleListen();
+  }, [isListening]);
+
+  const findTrack = (dispatch, e) => {
     e.preventDefault();
 
     axios
       .get(
         `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?
-        q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+        q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then((res) => {
         dispatch({
           type: 'SEARCH_TRACKS',
           payload: res.data.message.body.track_list,
         });
-        this.setState({ trackTitle: '' });
+        setTrackTitle('');
+        setIsListing(false);
       })
       .catch((err) => console.log(err));
   };
 
-  inputChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const inputChange = (e) => {
+    setTrackTitle(e.target.value);
   };
 
-  render() {
-    return (
-      <Consumer>
-        {(value) => {
-          const { dispatch } = value;
-          return (
-            <div className="card card-body mb-4 p-4">
-              <h1 className="display-4 text-center">
-                <i className="fas fa-music"></i> Search For A Song
-              </h1>
-              <p className="lead text-center">Get the lyrics for any song</p>
-              <form onSubmit={this.findTrack.bind(this, dispatch)}>
-                <div className="form-group">
+  const handleListen = () => {
+    if (isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log('continue..');
+        mic.start();
+      };
+    } else {
+      mic.stop();
+      mic.onend = () => {
+        console.log('Stopped Mic on Click');
+      };
+    }
+    mic.onstart = () => {
+      console.log('Mics on');
+    };
+
+    mic.onresult = (event) => {
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join('');
+      console.log(transcript);
+      setTrackTitle(transcript);
+      mic.onerror = (event) => {
+        console.log(event.error);
+      };
+    };
+  };
+
+  return (
+    <Consumer>
+      {(value) => {
+        const { dispatch } = value;
+        return (
+          <div className="card card-body mb-4 p-4">
+            <h1 className="display-4 text-center">
+              <i className="fas fa-music"></i> Search For A Song
+            </h1>
+            <p className="lead text-center">Get the lyrics for any song</p>
+            <form onSubmit={findTrack.bind(this, dispatch)}>
+              <div className="form-group">
+                <div className="input-group">
                   <input
                     autoComplete="off"
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Song title..."
                     name="trackTitle"
-                    value={this.state.trackTitle}
-                    onChange={this.inputChange}
+                    value={trackTitle}
+                    onChange={inputChange}
                   />
+                  <div
+                    className="input-group-append"
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => setIsListing((prevState) => !prevState)}
+                  >
+                    <span className="input-group-text">
+                      {isListening ? (
+                        <img
+                          src={Voice}
+                          alt="micIcon"
+                          style={{ height: '34px' }}
+                        />
+                      ) : (
+                        <img
+                          src={Mic}
+                          alt="voiceIcon"
+                          style={{ height: '34px' }}
+                        />
+                      )}
+                    </span>
+                  </div>
                 </div>
-                <button
-                  className="btn btn-primary btn-lg btn-block mb-5"
-                  type="submit"
-                >
-                  Get Track Lyrics
-                </button>
-              </form>
-            </div>
-          );
-        }}
-      </Consumer>
-    );
-  }
+              </div>
+              <button
+                className="btn btn-primary btn-lg btn-block mb-5"
+                type="submit"
+              >
+                Get Track Lyrics
+              </button>
+            </form>
+          </div>
+        );
+      }}
+    </Consumer>
+  );
 }
 
-export default Search;
+export default Search1;
